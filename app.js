1e85ab040c9f6fcd34a6ff9e9890c547dd25a79b
Bob Brown <robertbernardbrown@gmail.com>
2019-06-16 19:46:18 -0400

    connects front and backend

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 'use strict';
 
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/express_backend', (req, res) => {
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+});
