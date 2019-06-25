4bdfc98a3b4b78431b4cbe375dd2f6d249969289
Bob Brown <robertbernardbrown@gmail.com>
2019-06-24 23:35:11 -0400

    deletes old firebase project and creates a new one and saves env vars to dotenv file

--- app.js
+++ app.js
@@ -5,14 +5,18 @@ const app = express();
 const port = process.env.PORT || 5000;
 const firebase = require("firebase");
 
+// config
+require('dotenv').config()
+const envVars = process.env;
+
 var firebaseConfig = {
-  apiKey: "AIzaSyA9kvQ2Rv0FdZstkT1ZLMlUDIsS1ZE1Amw",
-  authDomain: "journalson-1e0a2.firebaseapp.com",
-  databaseURL: "https://journalson-1e0a2.firebaseio.com",
-  projectId: "journalson-1e0a2",
-  storageBucket: "",
-  messagingSenderId: "449163688400",
-  appId: "1:449163688400:web:9ce5af1b475d829c"
+  apiKey: envVars.apiKey,
+  authDomain: envVars.authDomain,
+  databaseURL: envVars.databaseURL,
+  projectId: envVars.projectId,
+  storageBucket: envVars.storageBucket,
+  messagingSenderId: envVars.messagingSenderId,
+  appId: envVars.appId
 };
 
 // Initialize Firebase
